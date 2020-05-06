7bd29a5d4d201ac7da097ffc5606a5ccf25c8e0b
Karolina Kowalczyk <karolinakowalczyk1999@gmail.com>
2020-05-06 21:10:48 +0200

    Update malfunctions class.

---
+++ malfunctions.py
@@ -0,0 +1,64 @@
+from random import randint
+
+class Malfunctions:
+    def __init__(self, name, status):
+        self.name = name
+        self.status = status
+        self.days = self.randomize_days()
+        self.hours = self.randomize_hours()
+        self.minutes = self.randomize_minutes()
+
+    def get_malname(self):
+        return self.name
+
+    def get_status(self):
+        return self.status
+
+    def get_days(self):
+        return self.days
+
+    def get_hours(self):
+        return self.hours
+
+    def get_minutes(self):
+        return self.minutes
+
+    def randomize_minutes(self):
+        minutes = randint(0, 59)
+        return minutes
+
+    def randomize_hours(self):
+        hours = randint(0, 23)
+        return hours
+
+    def randomize_days(self):
+        if self.status == "easy":
+            days = 0
+            return days
+        elif self.status == "medium":
+            days = randint(1, 3)
+            return days
+        elif self.status == "serious":
+            days = randint(4, 6)
+            return days
+        else:
+            print("I can't randomize time for unknown status.")
+
+
+mal1 = Malfunctions("Zerwanie linii", "easy")
+mal2 = Malfunctions("Zerwanie 3 linii", "medium")
+mal3 = Malfunctions("Zerwanie więcej niż 3 linii", "serious")
+mal4 = Malfunctions("Awaria sieciowa", "easy")
+mal5 = Malfunctions("Awaria zasilania", "medium")
+mal6 = Malfunctions("Brak surowców", "serious")
+mal7 = Malfunctions("Wybuch reaktora xD", "serious")
+
+
+mal_list = []
+mal_list.append(mal1)
+mal_list.append(mal2)
+mal_list.append(mal3)
+mal_list.append(mal4)
+mal_list.append(mal5)
+mal_list.append(mal6)
+mal_list.append(mal7)
