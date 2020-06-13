7cb7eee8079261830d7a31e5a6154812a9f3cb92
Karolina <karolinakowalczyk1999@gmail.com>
2020-06-13 12:12:09 +0200

    paramethes update

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from TestFunctions import TestFunctions
 
 pp = PowerPlant()
 
-pp.generateLines(3, 5)
-pp.generateWorkers(3)
+pp.generateLines(2, 20)
+pp.generateWorkers(10)
 
 with open('workers_efficient_before.csv', 'w', encoding='utf-8') as csvfile:
     csvwriter = csv.writer(csvfile)
     for i in range(0, len(pp.workers)):
         csvwriter.writerow(str(pp.workers[i].get_eff()))
 
-pp.runSimulation(20, 40)
+pp.runSimulation(10, 10*24*60)
 
 with open('workers_efficient_after.csv', 'w', encoding='utf-8') as csvfile:
     csvwriter = csv.writer(csvfile)
