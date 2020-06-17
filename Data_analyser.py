2d41b24481a1cbdcb4a6b6540b2c8b248950a965
Droniu <dron.official@yahoo.com>
2020-06-17 06:00:27 +0200

    aktualizacja Data_analyser.py

--- Data_analyser.py
+++ Data_analyser.py
@@ -2,6 +2,7 @@ import csv
 import copy
 import matplotlib.pyplot as plt
 import numpy as np
+import statistics as sts
 
 class Data_analyser:
 
@@ -9,48 +10,50 @@ class Data_analyser:
         self.simulations = list()
         self.workersBefore = list()
         self.workersAfter = list()
+        # self.
 
-    def load_lifetimes(self, simulations_count:int, lines_count:int, poles:int):
-        #initiates lists on which time will be loaded
+    def load_lifetimes(self, simulations_count: int, lines_count: int, poles: int):
+        # initiates lists on which time will be loaded
 
         lines = list()
-        for i in range(0,lines_count):
+        for i in range(0, lines_count):
             posts = list()
-            for j in range(0,poles):
+            for j in range(0, poles):
                 posts.append(0)
             lines.append(posts)
         for i in range(0, simulations_count):
-            #deepcopy to avoid issues with refferencing same list
+            # deepcopy to avoid issues with referencing same list
             self.simulations.append(copy.deepcopy(lines))
 
-        #open files and initiate counters
-        with open("poles_life_times.csv") as csv_file:    
+        # open files and initiate counters
+        with open("poles_life_times.csv") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             current_sim = -1
             current_line = 0
             current_post = 0
             for row in csv_reader:
-                #check if new simulation
+                # check if new simulation
                 if row == ['###']:
-                    current_sim +=1
+                    current_sim += 1
                     if current_sim == simulations_count:
                         break
-                #check if new Line
+                # check if new Line
                 elif row[0] == 'Linia':
                     current_line = int(row[1])
                     current_post = 0
                 else:
-                #update [sim][line][pole]
-                    #print("sim:", current_sim, "current post: ",current_post, "value: ", int(row[0]))
+                    # update [sim][line][pole]
+                    # print("sim:", current_sim, "current post: ",current_post, "value: ", int(row[0]))
                     self.simulations[current_sim][current_line][current_post] = int(row[0])
                     current_post += 1
-            #if you want to see the data from n-th simulation print simulations[n]
-            #print(self.simulations[0])
+            # if you want to see the data from n-th simulation print simulations[n]
+            # print(self.simulations[0])
 
-        #loads info about worker efficiency before and after last simulation
+        # loads info about worker efficiency before and after last simulation
+
     def loadWorkers(self):
         with open('workers_efficient_before.csv') as csvfile:
-            csv_reader = csv.reader(csvfile, delimiter =',')
+            csv_reader = csv.reader(csvfile, delimiter=',')
             for row in csv_reader:
                 self.workersBefore.append(int(row[0]))
 
@@ -58,32 +61,31 @@ class Data_analyser:
             csv_reader = csv.reader(csvfile, delimiter=',')
             for row in csv_reader:
                 self.workersAfter.append(int(row[0]))
-        #if you want to see the data
-        #print(self.workersBefore)
-        #print(self.workersAfter)
+        # if you want to see the data
+        # print(self.workersBefore)
+        # print(self.workersAfter)
 
-
-    def generate_lifetime_histogram(self, simulations:int, pole:int):
-        #validate data, if given value is too big, take max value instead
+    def generate_lifetime_histogram(self, simulations: int, pole: int):
+        # validate data, if given value is too big, take max value instead
         if len(self.simulations) < simulations:
-            simulations = len(self.simulations)-1
+            simulations = len(self.simulations) - 1
         if pole > len(self.simulations[0]):
-            pole = len(self.simulations[0][0])-1
+            pole = len(self.simulations[0][0]) - 1
 
-        #labels - nr of simulation
-        x_labels = range(1,simulations+1)
-        #converting to numpy array
+        # labels - nr of simulation
+        x_labels = range(1, simulations + 1)
+        # converting to numpy array
         x = np.arange(len(x_labels))
-        #each x tick is labeled
+        # each x tick is labeled
         correction = 0.5
-        plt.xticks(x+correction, x_labels)
-        #lifetimes of given pole in n simulations
-        y = [self.simulations[i][0][pole] for i in range(0,simulations)]
+        plt.xticks(x + correction, x_labels)
+        # lifetimes of given pole in n simulations
+        y = [self.simulations[i][0][pole] for i in range(0, simulations)]
         yMax = max(y)
-        #making bar chart with x from 0 to simulations and y from 0 to yMax*1.25
-        plt.bar(x+correction, y, label='Czas dzialania')
-        plt.axis([0, simulations, 0, yMax*1.25])
-        #metadata
+        # making bar chart with x from 0 to simulations and y from 0 to yMax*1.25
+        plt.bar(x + correction, y, label='Czas dzialania')
+        plt.axis([0, simulations, 0, yMax * 1.25])
+        # metadata
         plt.xlabel('Nr proby')
         plt.ylabel('Czas dzialania slupka')
         title = 'Czas życia słupka nr ' + str(pole) + ' w ' + str(simulations) + ' probach [min]'
@@ -92,22 +94,100 @@ class Data_analyser:
 
     def generate_workers_comparison(self):
 
-        labels = [i for i in range(1, len(self.workersBefore)+1)]
+        labels = [i for i in range(1, len(self.workersBefore) + 1)]
         x = np.arange(len(labels))
         yMax = max(max(self.workersBefore), max(self.workersAfter))
         width = 0.35
 
-        #placing labels on x axis
+        # placing labels on x axis
         plt.xticks(x + width, labels)
-        #fix: chart didnt fit
+        # fix: chart didnt fit
         correction = 0.2
-        #placing bars
-        plt.bar(x+correction, self.workersBefore, width, label='Efektywnosc przed symulacjami')
-        plt.bar(x+width+correction, self.workersAfter, width, label='Efektywnosc po symulacjach')
-        #metadata
+        # placing bars
+        plt.bar(x + correction, self.workersBefore, width, label='Efektywnosc przed symulacjami')
+        plt.bar(x + width + correction, self.workersAfter, width, label='Efektywnosc po symulacjach')
+        # metadata
         plt.legend()
         plt.title("Porownanie efektywnosci pracownikow")
         plt.xlabel("Nr pracownika")
         plt.ylabel("Efektywnosc pracownika [%]")
-        plt.axis([0, len(self.workersAfter), 0, yMax*1.25])
+        plt.axis([0, len(self.workersAfter), 0, yMax * 1.25])
+        plt.show()
+
+    def stdev_pole(self, simulations: int, poles_per_line: int, pole):
+        # validate data, if given value is too big, take max value instead
+        if len(self.simulations) < simulations:
+            simulations = len(self.simulations) - 1
+        if pole > poles_per_line:
+            pole = poles_per_line - 1
+        # TODO: pomijamy wyniki z drugiej linii?
+        # lifetimes of given pole in n simulations
+        y = [self.simulations[i][0][pole] for i in range(0, simulations)]
+        y_stdev = sts.stdev(y)
+        return y_stdev
+
+    def generate_poles_stdev(self, simulations: int, poles_per_line: int):
+
+        y = [self.stdev_pole(simulations, poles_per_line, i) for i in range(0, poles_per_line)]
+
+        # labels - nr of pole
+        x_labels = range(1, poles_per_line + 1)
+        # converting to numpy array
+        x = np.arange(len(x_labels))
+        # each x tick is labeled
+        correction = 0.5
+        plt.xticks(x + correction, x_labels)
+
+        # making bar chart with x from 0 to poles_per_line and y from 0 to yMax*1.25
+        plt.bar(x + correction, y)
+        plt.axis([0, poles_per_line, 0, 1.25 * max(y)])
+
+        # metadata
+        plt.legend()
+        plt.title("Odchylenie standardowe zycia slupkow dla " + str(simulations) + " symulacji")
+        plt.xlabel("Nr slupka")
+        plt.ylabel("ODchylenie standardowe")
+        plt.axis([0, poles_per_line, 0, max(y) * 1.25])
+        plt.show()
+
+    def avg_life_expectancy(self, simulations: int, poles_per_line: int, pole: int):
+        # validate data, if given value is too big, take max value instead
+        if len(self.simulations) < simulations:
+            simulations = len(self.simulations) - 1
+        if pole > poles_per_line:
+            pole = poles_per_line - 1
+        # TODO: pomijamy wyniki z drugiej linii?
+        # lifetimes of given pole in n simulations
+        y = [self.simulations[i][0][pole] for i in range(0, simulations)]
+        y_avg = np.average(y)
+        return y_avg
+
+    def total_avg(self, simulations: int, poles_per_line: int):
+        y = [self.avg_life_expectancy(simulations, poles_per_line, i) for i in range(0, poles_per_line)]
+        return np.average(y)
+
+    def generate_poles_life_expectancy(self, simulations: int, poles_per_line: int):
+
+        # highly advanced two-dimensional array with nested loops which probably doesn't work
+
+        y = [self.avg_life_expectancy(simulations, poles_per_line, i) for i in range(0, poles_per_line)]
+
+        # labels - nr of pole
+        x_labels = range(1, poles_per_line + 1)
+        # converting to numpy array
+        x = np.arange(len(x_labels))
+        # each x tick is labeled
+        correction = 0.5
+        plt.xticks(x + correction, x_labels)
+
+        # making bar chart with x from 0 to poles_per_line and y from 0 to yMax*1.25
+        plt.bar(x + correction, y, label='Czas dzialania')
+        plt.axis([0, poles_per_line, 0, 1.25 * max(y)])
+
+        # metadata
+        plt.legend()
+        plt.title("Dlugosc zycia slupkow dla " + str(simulations) + " symulacji")
+        plt.xlabel("Nr slupka")
+        plt.ylabel("Sredni czas zycia [s]")
+        plt.axis([0, poles_per_line, 0, max(y) * 1.25])
         plt.show()
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import csv
 from Data_analyser import Data_analyser
 
 case = input("If you want to simulate enter 1, if you want to analyze data enter 2. \nIf you want to quit enter 0.\n")
-#how many sims you want to run/import
+# how many sims you want to run/import
 sims = 20
+lines = 2
+poles = 20
 case = int(case)
 if case == 1:
     pp = PowerPlant()
-    pp.generateLines(2, 20)
+    pp.generateLines(lines, poles)
     pp.generateWorkers(10)
 
     with open('workers_efficient_before.csv', 'w', encoding='utf-8', newline='') as csvfile:
@@ -16,12 +18,12 @@ if case == 1:
         for i in range(0, len(pp.workers)):
             csvwriter.writerow([str(pp.workers[i].get_eff())])
 
-    #printuje ### przed kazda symulacja
+    # printuje ### przed kazda symulacja
     for i in range(0, sims):
         with open('poles_life_times.csv', 'a', encoding='utf-8', newline='') as csvfile2:
             csvwriter = csv.writer(csvfile2)
             csvwriter.writerow(['###'])
-        pp.runSimulation(10, 10*24*60)
+        pp.runSimulation(10, 10 * 24 * 60)
 
     with open('workers_efficient_after.csv', 'w', encoding='utf-8', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
@@ -30,10 +32,12 @@ if case == 1:
 
 elif case == 2:
     da = Data_analyser()
-    da.load_lifetimes(sims, 2, 20)
+    da.load_lifetimes(sims, lines, poles)
     da.generate_lifetime_histogram(sims, 10)
     da.loadWorkers()
     da.generate_workers_comparison()
-
+    da.generate_poles_life_expectancy(sims, poles)
+    da.generate_poles_stdev(sims, poles)
+    print("\nSredni czas zycia wszystkich slupkow: " + str(da.total_avg(sims, poles)) + " s")
 elif case == 0:
     SystemExit
